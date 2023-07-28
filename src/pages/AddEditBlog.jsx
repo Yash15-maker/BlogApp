260c96faa864179095695acf3eff3204e670d892
Yash Mishra <64361746+Yash15-maker@users.noreply.github.com>
2023-07-28 13:17:44 +0530

    Update AddEditBlog.jsx
    Added the authentication for Inputs

--- src/pages/AddEditBlog.jsx
+++ src/pages/AddEditBlog.jsx
@@ -1,67 +1,73 @@
-import React, { useState } from 'react'
-import { Timestamp, collection, addDoc } from 'firebase/firestore'
-import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage'
-import { storage, db, auth } from '../Firebase'
-import { toast } from 'react-toastify'
-import { useAuthState } from 'react-firebase-hooks/auth'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { Timestamp, collection, addDoc } from "firebase/firestore";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { storage, db, auth } from "../Firebase";
+import { toast } from "react-toastify";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Link } from "react-router-dom";
 
-export default function AddArticle () {
-  const [user] = useAuthState(auth)
+export default function AddArticle() {
+  const [user] = useAuthState(auth);
   const [formData, setFormData] = useState({
-    title: '',
-    message: '',
-    image: '',
-    createdAt: Timestamp.now().toDate()
-  })
+    title: "",
+    message: "",
+    image: "",
+    createdAt: Timestamp.now().toDate(),
+  });
+  const [progress, setProgress] = useState(0);
 
-  const [progress, setProgress] = useState(0)
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
-  const handleChange = e => {
-    setFormData({ ...formData, [e.target.name]: e.target.value })
-  }
-
-  const handleImageChange = e => {
-    setFormData({ ...formData, image: e.target.files[0] })
-  }
+  const handleImageChange = (e) => {
+    setFormData({ ...formData, image: e.target.files[0] });
+  };
 
   const handlePublish = () => {
     if (!formData.title || !formData.message || !formData.image) {
-      alert('Please fill all the fields');
-      setFormData({
-        title: '',
-        message: '',
-        image: '',
-      })
-      window.location.reload()
-      return
+      alert("Please fill all the fields");
+
+      return;
     }
+    const wordCount = formData.message
+      .split(" ")
+      .filter((word) => word !== "").length;
+    if (wordCount < 100) {
+      alert("Enter more than 100 words");
+      return;
+    }
+    setFormData({
+      title: "",
+      message: "",
+      image: "",
+    });
 
     const storageRef = ref(
       storage,
       `/images/${Date.now()}${formData.image.name}`
-    )
-    const uploadImage = uploadBytesResumable(storageRef, formData.image)
+    );
+    const uploadImage = uploadBytesResumable(storageRef, formData.image);
     uploadImage.on(
-      'state_changed',
-      snapshot => {
+      "state_changed",
+      (snapshot) => {
         const progressPercent = Math.round(
           (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-        )
-        setProgress(progressPercent)
+        );
+        setProgress(progressPercent);
       },
-      err => {
-        console.log(err)
+      (err) => {
+        console.log(err);
       },
       () => {
         setFormData({
-          title: '',
-          description: '',
-          image: ''
-        })
+          title: "",
+          description: "",
+          image: "",
+        });
 
-        getDownloadURL(uploadImage.snapshot.ref).then(url => {
-          const articleRef = collection(db, 'blogs')
+        getDownloadURL(uploadImage.snapshot.ref).then((url) => {
+          const articleRef = collection(db, "blogs");
           addDoc(articleRef, {
             Title: formData.title,
             Message: formData.message,
@@ -70,64 +76,64 @@ export default function AddArticle () {
             createdBy: user.displayName,
             userId: user.uid,
             likes: [],
-            comments: []
+            comments: [],
           })
             .then(() => {
-              toast('Article added successfully', { type: 'success' })
-              setProgress(0)
+              toast("Article added successfully", { type: "success" });
+              setProgress(0);
             })
-            .catch(err => {
-              toast('Error adding article', { type: 'error' })
-            })
-        })
+            .catch((err) => {
+              toast("Error adding article", { type: "error" });
+            });
+        });
       }
-    )
-  }
+    );
+  };
 
   return (
-    <div className='border p-3 mt-3 bg-light' >
+    <div className="border p-3 mt-3 bg-light">
       {!user ? (
         <>
           <h2>
-            <Link to='/signin'>Login</Link>
+            <Link to="/signin">Login</Link>
           </h2>
-          Don't have an account? <Link to='/register'>Signup</Link>
+          Don't have an account? <Link to="/register">Signup</Link>
         </>
       ) : (
         <>
           <h2>Create article</h2>
-          <div className='form-group'>
-            <label htmlFor=''>Title</label>
+          <div className="form-group">
+            <label htmlFor="">Title</label>
             <input
-              type='text'
-              name='title'
-              className='form-control'
+              type="text"
+              name="title"
+              className="form-control"
               value={formData.title}
-              onChange={e => handleChange(e)}
+              onChange={(e) => handleChange(e)}
             />
           </div>
 
-          <label htmlFor=''>Description</label>
+          <label htmlFor="">Description</label>
           <textarea
-            name='message'
-            className='form-control'
+            name="message"
+            className="form-control"
             value={formData.message}
-            onChange={e => handleChange(e)}
+            onChange={(e) => handleChange(e)}
           />
 
-          <label htmlFor=''>Image</label>
+          <label htmlFor="">Image</label>
           <input
-            type='file'
-            name='image'
-            accept='image/*'
-            className='form-control'
-            onChange={e => handleImageChange(e)}
+            type="file"
+            name="image"
+            accept="image/*"
+            className="form-control"
+            onChange={(e) => handleImageChange(e)}
           />
 
           {progress === 0 ? null : (
-            <div className='progress'>
+            <div className="progress">
               <div
-                className='progress-bar progress-bar-striped mt-2'
+                className="progress-bar progress-bar-striped mt-2"
                 // style={{ width: `${progress}%` }}
               >
                 {`uploading image ${progress}%`}
@@ -135,7 +141,7 @@ export default function AddArticle () {
             </div>
           )}
           <button
-            className='form-control btn-primary mt-2'
+            className="form-control btn-primary mt-2"
             onClick={handlePublish}
           >
             Publish
@@ -143,5 +149,5 @@ export default function AddArticle () {
         </>
       )}
     </div>
-  )
+  );
 }
