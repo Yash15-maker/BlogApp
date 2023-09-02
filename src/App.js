9e40cd5a575e8081050d7fad4bc2b9988df95552
Yash Mishra <64361746+Yash15-maker@users.noreply.github.com>
2023-09-03 02:04:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,46 @@
-import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { Container } from '@mui/material'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import Home from './pages/Home'
-import AddEditBlog from './pages/AddEditBlog'
-import ArticlesPage from './pages/ArticlesPage'
-import Header from './pages/Header'
-import Update from './pages/Update'
+
+
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Container } from "@mui/material";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import AddEditBlog from "./pages/AddEditBlog";
+import ArticlesPage from "./pages/ArticlesPage";
+import Header from "./pages/Header";
+import Update from "./pages/Update";
+import ProfileCard from "./pages/ProfileCard";
+import React, { useState } from "react";
 
 // import Header from './pages/Header'
-function App () {
+function App() {
   return (
-    <div className='App'>
-      <Container>
-        <Router>
-          <Header />
+    <div className="App">
+      <Router>
+        <Header />
+        <Container>
           <Routes>
-            <Route path='/register' element={<Register />} />
-            <Route path='/signin' element={<Login />} />
-            <Route path='/update/:id' element={<Update />} />
-            <Route path='/article/:id' element={<ArticlesPage />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/signin" element={<Login />} />
+            <Route path="/update/:id" element={<Update />} />
+            <Route path="/article/:id" element={<ArticlesPage />} />
             <Route
               exact
-              path='/'
+              path="/"
               element={
-                <div className='row mt-4'>
+                <div className="row mt-4">
                   <AddEditBlog />
                   <Home />
                 </div>
               }
             />
+            <Route path="/profile" element={<ProfileCard />} />
           </Routes>
-        </Router>
-      </Container>
+        </Container>
+      </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
